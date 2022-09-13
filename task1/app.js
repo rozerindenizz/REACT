c0abc11bb8e90c9d8f486d8ec2ed2f1ad09d3830
rozerindenizz <71177667+rozerindenizz@users.noreply.github.com>
2022-09-14 00:00:41 +0300

    Add files via upload

---
+++ task1/app.js
@@ -0,0 +1,12 @@
+
+import axios from "axios";
+
+const getData = async (id)=> {
+
+const {data : user} = await axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+
+const {data : posts} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+
+console.log({user, posts});
+}
+export default getData;
